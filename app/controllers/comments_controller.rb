65bd7d854b4ad43b5a742c6f92b4538a4fdfbb91
Rigz777 <rigzgoodmusic@gmail.com>
2017-07-15 22:14:33 -0400

    completed layout excluding media queries

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,13 @@ class CommentsController < ApplicationController
     end
   end
 
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
   private
 
   def find_post
